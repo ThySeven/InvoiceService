0839ff42d10953b62894b1ec86c655890d0e2c73
Wufferman <eaajbst@students.eaaa.dk>
2024-05-08 13:18:23 +0200

    dummy invoice

--- InvoiceService/Repositorys/InvoiceRepository.cs
+++ InvoiceService/Repositorys/InvoiceRepository.cs
@@ -13,11 +13,111 @@ namespace InvoiceService.Repositorys
         private string _connectionString = Environment.GetEnvironmentVariable("MongoDBConnectionString");
         IAuctionCoreQueue queue;
 
+        String InvoiceHtml = @"<!DOCTYPE html>
+<html lang=""en"">
+<head>
+<meta charset=""UTF-8"">
+<meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+<title>Invoice</title>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    max-width: 800px;
+    margin: 40px auto;
+    padding: 20px;
+    border: 1px solid #eee;
+    border-radius: 10px;
+  }
+  .company-address, .invoice-details {
+    margin-bottom: 20px;
+  }
+  .company-address h2, .invoice-details h2 {
+    margin: 0 0 10px 0;
+  }
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  table, th, td {
+    border: 1px solid black;
+  }
+  th, td {
+    padding: 10px;
+    text-align: left;
+  }
+  .signature {
+    margin-top: 40px;
+  }
+  .signature img {
+    width: 150px;
+  }
+  .total-amount {
+    text-align: right;
+    margin-top: 20px;
+  }
+</style>
+</head>
+<body>
+
+<div class=""company-address"">
+  <h2>Grøn & Olsen</h2>
+  <p>Sønderhøj 30, 8260 Viby J</p>
+  <p>Phone: 88 88 88 88</p>
+  <p>Email: grønogolsen@gmail.com</p>
+</div>
+
+<div class=""invoice-details"">
+  <h2>Invoice #12345</h2>
+  <p>Invoice Date: 2024-05-07</p>
+  <p>Due Date: 2024-05-09</p>
+</div>
+
+<table>
+  <thead>
+    <tr>
+      <th>Description</th>
+      <th>Quantity</th>
+      <th>Price</th>
+      <th>Total</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Havetraktor</td>
+      <td>2</td>
+      <td>$100.00</td>
+      <td>$200.00</td>
+    </tr>
+    <tr>
+      <td>Bagger 293</td>
+      <td>5</td>
+      <td>$20.00</td>
+      <td>$100.00</td>
+    </tr>
+    <tr>
+      <td colspan=""3"" style=""text-align:right;""><strong>Grand Total</strong></td>
+      <td><strong>$300.00</strong></td>
+    </tr>
+  </tbody>
+</table>
+
+<div class=""total-amount"">
+  <h2>Total Due: $300.00</h2>
+</div>
+
+<div class=""signature"">
+  <p>Authorized Signature</p>
+  <img src=""https://scontent.fcph5-1.fna.fbcdn.net/v/t39.30808-6/441856076_122119247390268251_6872370368606673835_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kumg7n8kOKsQ7kNvgE--SRI&_nc_ht=scontent.fcph5-1.fna&oh=00_AfAofBAZoBPMHwwfex41mYJO0_s_qnRfTl91buhLlmz3MQ&oe=664113D0"" alt=""Signature"">
+  <p>Kell Olsen, CEO</p>
+</div>
+
+</body>
+</html>";
+
         AutoMail mail = new AutoMail()
         {
             DateTime = DateTime.Now,
             SenderMail = "gronogolsen@gmail.com",
-            Content = "This is your invoice, yoyo<br /><img src=\"https://scontent.fcph5-1.fna.fbcdn.net/v/t39.30808-6/441856076_122119247390268251_6872370368606673835_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kumg7n8kOKsQ7kNvgE--SRI&_nc_ht=scontent.fcph5-1.fna&oh=00_AfAofBAZoBPMHwwfex41mYJO0_s_qnRfTl91buhLlmz3MQ&oe=664113D0\"/>",
             Header = "GrønOgOlsen Invoice - Betal!"
         };
 
@@ -27,6 +127,13 @@ namespace InvoiceService.Repositorys
             var client = new MongoClient(_connectionString);
             var database = client.GetDatabase("AuctionCoreServices");
             _invoices = database.GetCollection<InvoiceModel>("Invoices");
+            mail = new AutoMail()
+            {
+                DateTime = DateTime.Now,
+                SenderMail = "gronogolsen@gmail.com",
+                Header = "GrønOgOlsen Invoice - Betal!",
+                Content = InvoiceHtml
+            };
         }
         public InvoiceRepository(IMongoDatabase db)
         {
--- InvoiceService/obj/Debug/net8.0/InvoiceService.AssemblyInfo.cs
+++ InvoiceService/obj/Debug/net8.0/InvoiceService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ed54acc10e43778d2fd91af84c13c506a49149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e6d003711894dc47aee78bbf0b90715177b7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
