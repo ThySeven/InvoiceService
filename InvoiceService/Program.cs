5392c7dd7c0323400816d00f554437de7e1b2a7b
Wufferman <eaajbst@students.eaaa.dk>
2024-05-22 09:04:51 +0200

    unwanted logging removed

--- InvoiceService/Program.cs
+++ InvoiceService/Program.cs
@@ -19,99 +19,94 @@ logger.Debug("init main");
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
 builder.Host.UseNLog();
-try
-{
-    var EndPoint = Environment.GetEnvironmentVariable("VAULT_IP");
-    var httpClientHandler = new HttpClientHandler();
-    httpClientHandler.ServerCertificateCustomValidationCallback =
-    (message, cert, chain, sslPolicyErrors) => { return true; };
 
-    IAuthMethodInfo authMethod =
-    new TokenAuthMethodInfo(Environment.GetEnvironmentVariable("VAULT_SECRET"));
-    // Initialize settings. You can also set proxies, custom delegates etc. here.
-    var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+var EndPoint = Environment.GetEnvironmentVariable("VAULT_IP");
+var httpClientHandler = new HttpClientHandler();
+httpClientHandler.ServerCertificateCustomValidationCallback =
+(message, cert, chain, sslPolicyErrors) => { return true; };
+
+IAuthMethodInfo authMethod =
+new TokenAuthMethodInfo(Environment.GetEnvironmentVariable("VAULT_SECRET"));
+// Initialize settings. You can also set proxies, custom delegates etc. here.
+var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+{
+    Namespace = "",
+    MyHttpClientProviderFunc = handler
+    => new HttpClient(httpClientHandler)
     {
-        Namespace = "",
-        MyHttpClientProviderFunc = handler
-        => new HttpClient(httpClientHandler)
+        BaseAddress = new Uri(EndPoint)
+    }
+};
+
+IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+Secret<SecretData> kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2
+.ReadSecretAsync(path: "jwt", mountPoint: "secret");
+var jwtSecret = kv2Secret.Data.Data["secret"];
+var jwtIssuer = kv2Secret.Data.Data["issuer"];
+var vaultjwtInternalApiKey = kv2Secret.Data.Data["internalApiKey"];
+
+
+string mySecret = Convert.ToString(jwtSecret) ?? "none";
+string myIssuer = Convert.ToString(jwtIssuer) ?? "none";
+string vaultInternalApiKey = Convert.ToString(vaultjwtInternalApiKey) ?? "none";
+WebManager.GetInstance.HttpClient.DefaultRequestHeaders.Add("X-Internal-ApiKey", vaultInternalApiKey);
+builder.Services
+.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = myIssuer,
+        ValidAudience = "http://localhost",
+        IssuerSigningKey =
+    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
+    };
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
         {
-            BaseAddress = new Uri(EndPoint)
+            //AuctionCoreLogger.Logger.Info($"Received API Call from {context.Request.Headers.Origin}");
+
+            if (context.Request.Headers.TryGetValue("X-Internal-ApiKey", out var extractedApiKey))
+            {
+                var internalApiKey = vaultInternalApiKey; // or fetch from configuration
+                if (internalApiKey.Equals(extractedApiKey))
+                {
+                    //AuctionCoreLogger.Logger.Info($"JWTBypass {context.Request.Headers.Origin}");
+                    context.HttpContext.Items["InternalRequest"] = true;
+
+                    // Skip JWT token processing
+                    context.Token = null;
+
+                    context.Response.Headers.Add("X-Auth-Skipped", "true");
+                }
+            }
+
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            if (context.HttpContext.Items.ContainsKey("InternalRequest"))
+            {
+                context.Success();
+            }
+            return Task.CompletedTask;
+        },
+        OnChallenge = context =>
+        {
+            if (context.HttpContext.Items.ContainsKey("InternalRequest"))
+            {
+                context.HandleResponse(); // This suppresses the default 401
+            }
+            return Task.CompletedTask;
         }
     };
+});
 
-    IVaultClient vaultClient = new VaultClient(vaultClientSettings);
-    Secret<SecretData> kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2
-    .ReadSecretAsync(path: "jwt", mountPoint: "secret");
-    var jwtSecret = kv2Secret.Data.Data["secret"];
-    var jwtIssuer = kv2Secret.Data.Data["issuer"];
-    var vaultjwtInternalApiKey = kv2Secret.Data.Data["internalApiKey"];
-
-
-    string mySecret = Convert.ToString(jwtSecret) ?? "none";
-    string myIssuer = Convert.ToString(jwtIssuer) ?? "none";
-    string vaultInternalApiKey = Convert.ToString(vaultjwtInternalApiKey) ?? "none";
-    WebManager.GetInstance.HttpClient.DefaultRequestHeaders.Add("X-Internal-ApiKey", vaultInternalApiKey);
-    builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = myIssuer,
-            ValidAudience = "http://localhost",
-            IssuerSigningKey =
-        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
-        };
-        options.Events = new JwtBearerEvents
-        {
-            OnMessageReceived = context =>
-            {
-                AuctionCoreLogger.Logger.Info($"Received API Call from {context.Request.Headers.Origin}");
-
-                if (context.Request.Headers.TryGetValue("X-Internal-ApiKey", out var extractedApiKey))
-                {
-                    var internalApiKey = vaultInternalApiKey; // or fetch from configuration
-                    if (internalApiKey.Equals(extractedApiKey))
-                    {
-                        AuctionCoreLogger.Logger.Info($"JWTBypass {context.Request.Headers.Origin}");
-                        context.HttpContext.Items["InternalRequest"] = true;
-
-                        // Skip JWT token processing
-                        context.Token = null;
-
-                        context.Response.Headers.Add("X-Auth-Skipped", "true");
-                    }
-                }
-
-                return Task.CompletedTask;
-            },
-            OnTokenValidated = context =>
-            {
-                if (context.HttpContext.Items.ContainsKey("InternalRequest"))
-                {
-                    context.Success();
-                }
-                return Task.CompletedTask;
-            },
-            OnChallenge = context =>
-            {
-                if (context.HttpContext.Items.ContainsKey("InternalRequest"))
-                {
-                    context.HandleResponse(); // This suppresses the default 401
-                }
-                return Task.CompletedTask;
-            }
-        };
-    });
-}
-catch(Exception ex)
-{
-    logger.Fatal($"JWT Failed error: {ex}");
-}
 // Add services to the container.
 
 builder.Services.AddAuthorization(options =>
